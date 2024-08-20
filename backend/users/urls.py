71a51133f45e5db599d37311e480fb6ffa6d2a69
Parvez Hossen <parvezhossen81@gmail.com>
2024-08-20 22:13:45 +0600

    developed user list api endpoint to add user, use sign-up api endpoints

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('/sign-up', views.UserSignUpAPIView.as_view(), name='user-register'),
+    path('/list',views.UserListAPIView.as_view(),name='user-list')
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from .models import User
 from .serializers import UserSerializer
 
 # Create your views here.
@@ -20,3 +21,10 @@ class UserSignUpAPIView(generics.CreateAPIView):
             'token': user.tokens(),
             'user':UserSerializer(user).data
         }, status=status.HTTP_201_CREATED)
+    
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        return queryset
